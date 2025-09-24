b6d357248941b1c3de8ee9cb48c8dd9fbacd6bd1
Smandalk <90818632+Smandalk@users.noreply.github.com>
2025-09-24 08:37:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# Keep track of room hosts
+room_hosts = {}
 
 @app.route('/game/<room>')
 def game(room):
@@ -14,18 +16,31 @@ def game(room):
 @socketio.on('join')
 def handle_join(data):
     room = data['room']
+    sid = data['sid']  # unique socket id of client
+
     join_room(room)
-    emit('message', {'msg': f"Joined room {room}"}, room=room)
+
+    # If no host assigned yet, this player becomes the host
+    if room not in room_hosts:
+        room_hosts[room] = sid
+        emit('host_status', {'is_host': True})
+        emit('message', {'msg': f"You are the host of room {room}"}, to=sid)
+    else:
+        emit('host_status', {'is_host': False})
+        emit('message', {'msg': f"Joined room {room}"}, to=sid)
 
 @socketio.on('generate_number')
 def handle_generate_number(data):
     room = data['room']
-    number = random.randint(1, 100)
-    emit('new_number', {'number': number}, room=room)
+    sid = data['sid']
+
+    # Only host can generate
+    if room_hosts.get(room) == sid:
+        number = random.randint(1, 100)
+        emit('new_number', {'number': number}, room=room)
+    else:
+        emit('message', {'msg': "Only the host can generate numbers!"}, to=sid)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-
     socketio.run(app, host='0.0.0.0', port=port)
-
-
